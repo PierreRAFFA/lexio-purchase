22aef21c77dce18dbeff94b604b475f06be9cb04
PierreRAFFA <pierre.raffa@gmail.com>
2017-10-04 14:55:55 +0100

    Update inapp purchase balance increase in database

--- server/boot/script.js
+++ server/boot/script.js
@@ -10,11 +10,11 @@ module.exports = function(app) {
 
   Product.destroyAll({}, function(err, count) {
       Product.create([
-        {store: 'apple', storeProductId: 'lexio.coin1', balance: '8', token: '0', price: 0.99},
-        {store: 'apple', storeProductId: 'lexio.coin2', balance: '16', token: '0', price: 1.99},
-        {store: 'apple', storeProductId: 'lexio.coin3', balance: '48', token: '0', price: 4.99},
-        {store: 'apple', storeProductId: 'lexio.coin4', balance: '100', token: '0', price: 9.99},
-        {store: 'apple', storeProductId: 'lexio.coin5', balance: '210', token: '0', price: 19.99},
+        {store: 'apple', storeProductId: 'lexio.coin1', balance: '16', token: '0', price: 0.99},
+        {store: 'apple', storeProductId: 'lexio.coin2', balance: '36', token: '0', price: 1.99},
+        {store: 'apple', storeProductId: 'lexio.coin3', balance: '100', token: '0', price: 4.99},
+        {store: 'apple', storeProductId: 'lexio.coin4', balance: '220', token: '0', price: 9.99},
+        {store: 'apple', storeProductId: 'lexio.coin5', balance: '500', token: '0', price: 19.99},
       ], function(err, products) {
         if (err) throw err;
         console.log('Created products:', products);
